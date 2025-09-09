8b6b5f7519a2615ef5027f2efa81bd7bb4c69360
nchopra <naomi.chopra.021@gmail.com>
2025-09-10 01:46:05 +0530

    fix: use hashhistory to ensured compiled app works

--- apps/desktop/src/renderer/main/content.tsx
+++ apps/desktop/src/renderer/main/content.tsx
@@ -1,11 +1,18 @@
 import React from "react";
-import { RouterProvider, createRouter } from "@tanstack/react-router";
+import {
+  RouterProvider,
+  createRouter,
+  createHashHistory,
+} from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
+
+const hashHistory = createHashHistory();
 
 // Create the router instance
 const router = createRouter({
   routeTree,
   defaultPreload: "intent",
+  history: hashHistory,
 });
 
 // Register the router instance for type safety
